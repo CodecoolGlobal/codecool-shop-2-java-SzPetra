bfa07fd3e9371d4e3800af7a201b19656740affd
papp <papp.csenge0@gmail.com>
2022-06-27 14:45:22 +0200

    feat: search products by supplier

---
+++ src/main/java/com/codecool/shop/controller/SupplierController.java
@@ -0,0 +1,43 @@
+package com.codecool.shop.controller;
+
+import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
+import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.dao.implementation.SupplierDaoMem;
+import com.codecool.shop.service.ProductService;
+import com.codecool.shop.service.SupplierService;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/supplier/"})
+public class SupplierController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
+        int supplierId = Integer.parseInt(request.getParameter("supplier_id"));
+
+        ProductDao productDataStore = ProductDaoMem.getInstance();
+        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+
+
+        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
+        SupplierService supplierService = new SupplierService(productDataStore, supplierDataStore);
+
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
+        WebContext context = new WebContext(request, resp, request.getServletContext());
+
+        context.setVariable("products", supplierService.getProductsBySupplier(supplierId));
+
+        engine.process("product/index.html", context, resp.getWriter());
+    }
+}
