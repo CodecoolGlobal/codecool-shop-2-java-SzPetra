95a4d676e2107ca3728172e36b42d3b716d7c476
cserdniel <cserdniel@hotmail.com>
2022-06-27 11:43:30 +0200

    Implement getTotalPrice()

--- src/main/java/com/codecool/shop/model/LineItem.java
+++ src/main/java/com/codecool/shop/model/LineItem.java
@@ -1,14 +1,18 @@
 package com.codecool.shop.model;
 
+import java.math.BigDecimal;
+
 public class LineItem {
 
     private final int productId;
     private final String productName;
+    private final BigDecimal unitPrice;
     private int quantity;
 
     public LineItem(Product product){
         this.productId = product.getId();
         this.productName = product.getName();
+        this.unitPrice = product.getDefaultPrice();
         this.quantity = 1;
     }
 
@@ -26,5 +30,9 @@ public class LineItem {
 
     public void setQuantity(int quantity) {
         this.quantity = quantity;
+    }
+
+    public BigDecimal getTotalPrice(){
+        return unitPrice.multiply(BigDecimal.valueOf(quantity));
     }
 }
