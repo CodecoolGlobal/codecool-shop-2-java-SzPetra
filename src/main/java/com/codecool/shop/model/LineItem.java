f1d89bffa0556dace5d8cef7693ea0ee8403bf5a
cserdniel <cserdniel@hotmail.com>
2022-06-27 12:13:10 +0200

    Implement equals() and hashCode()

--- src/main/java/com/codecool/shop/model/LineItem.java
+++ src/main/java/com/codecool/shop/model/LineItem.java
@@ -1,6 +1,7 @@
 package com.codecool.shop.model;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class LineItem {
 
@@ -38,5 +39,18 @@ public class LineItem {
 
     public boolean compareToProduct(Product product){
         return productId == product.getId();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LineItem lineItem = (LineItem) o;
+        return productId == lineItem.productId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productId);
     }
 }
