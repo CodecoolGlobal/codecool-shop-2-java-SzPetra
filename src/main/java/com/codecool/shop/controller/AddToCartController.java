797ed7ba51691778a4cf6e3fd97745e297c2d44a
cserdniel <cserdniel@hotmail.com>
2022-06-28 13:56:24 +0200

    Fix url path to add-to-cart

--- src/main/java/com/codecool/shop/controller/AddToCartController.java
+++ src/main/java/com/codecool/shop/controller/AddToCartController.java
@@ -20,7 +20,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = {"/add-to-cart/"})
+@WebServlet(urlPatterns = {"/add-to-cart"})
 public class AddToCartController extends HttpServlet {
 
     @Override
