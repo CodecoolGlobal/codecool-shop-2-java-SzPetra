e48ac031167eb40568531bb0ec0c2b2470230f12
papp <papp.csenge0@gmail.com>
2022-06-28 10:11:44 +0200

    feat: get all products in card by clicking show cart button

--- src/main/java/com/codecool/shop/controller/CartController.java
+++ src/main/java/com/codecool/shop/controller/CartController.java
@@ -1,8 +1,10 @@
 package com.codecool.shop.controller;
 
 import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.CartDao;
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.implementation.CartDaoMem;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
 import com.codecool.shop.service.ProductService;
@@ -16,19 +18,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = {"/cart/"})
+@WebServlet(urlPatterns = {"/cart"})
 public class CartController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         ProductDao productDataStore = ProductDaoMem.getInstance();
-        //ProductService productService = new ProductService(productDataStore,productCategoryDataStore);
+        CartDao cartDataStore = CartDaoMem.getInstance();
+
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
         //context.setVariable("category", productService.getProductCategory(categoryId));
         //context.setVariable("products", productService.getProductsForCategory(categoryId));
+
+        System.out.println(cartDataStore.getAll());
 
         engine.process("product/index.html", context, resp.getWriter());
     }
