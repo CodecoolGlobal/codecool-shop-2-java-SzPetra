e134c07113cc27c2bb2e66e2a010252d3d41f25f
cserdniel <cserdniel@hotmail.com>
2022-06-28 13:00:27 +0200

    Add addCartHandler ref to evet listener

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -2,7 +2,7 @@ function init(){
     const addCartButtonContainers = document.querySelectorAll(".card-button-container");
     for(let container of addCartButtonContainers){
         let button = container.querySelector("a");
-        button.addEventListener("click", (e)=>{console.log(e.currentTarget.id)});
+        button.addEventListener("click",addCartHandler);
     }
 }
 
