fad38ece21f44fbd4597dd42dee2038aeb4c3ee6
cserdniel <cserdniel@hotmail.com>
2022-06-29 10:45:34 +0200

    Add dao field and constructor

--- src/main/java/com/codecool/shop/service/CartService.java
+++ src/main/java/com/codecool/shop/service/CartService.java
@@ -1,4 +1,17 @@
 package com.codecool.shop.service;
 
+import com.codecool.shop.dao.CartDao;
+import com.codecool.shop.dao.ProductDao;
+
 public class CartService {
+
+    private CartDao cartDao;
+    private ProductDao productDao;
+
+    public CartService(CartDao cartDao, ProductDao productDao) {
+        this.cartDao = cartDao;
+        this.productDao = productDao;
+    }
+
+
 }
