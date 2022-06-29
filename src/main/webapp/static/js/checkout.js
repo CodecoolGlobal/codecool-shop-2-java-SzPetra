7f9e27c98bd35a45ce44363daa78237f42f7dd37
SzPetra <petra.szilagyi27@gmail.com>
2022-06-29 13:10:27 +0200

    feat: implement api route for saving orders

--- src/main/webapp/static/js/checkout.js
+++ src/main/webapp/static/js/checkout.js
@@ -6,7 +6,18 @@ function init() {
 
 async function handleCheckoutData() {
 
-    alert("working");
+    //checkout form datas
+    let userName = document.getElementById("fname").value;
+    let email = document.getElementById("email").value;
+    let address = document.getElementById("adr").value;
+    let city = document.getElementById("city").value;
+    let state = document.getElementById("state").value;
+    let zipCode = document.getElementById("zip").value;
+    let cardName = document.getElementById("cname").value;
+    let cardNumber = document.getElementById("ccnum").value;
+    let expMonth = document.getElementById("expmonth").value;
+    let expYear = document.getElementById("expyear").value;
+    let cvv = document.getElementById("cvv").value;
 }
 
 init()
