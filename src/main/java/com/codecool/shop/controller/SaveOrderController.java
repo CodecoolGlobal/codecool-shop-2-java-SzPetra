357c26103f0f5430eb521c0976a044196d60fda0
SzPetra <petra.szilagyi27@gmail.com>
2022-06-29 13:47:37 +0200

    feat: api post function implement in js, 500 error message

--- src/main/java/com/codecool/shop/controller/SaveOrderController.java
+++ src/main/java/com/codecool/shop/controller/SaveOrderController.java
@@ -15,6 +15,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(urlPatterns = {"/api/save/order"})
 public class SaveOrderController extends HttpServlet {
@@ -22,5 +23,9 @@ public class SaveOrderController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        String userName = request.getParameter("userName").toString();
+        PrintWriter pw = response.getWriter();
+        pw.write(userName);
+        System.out.println(userName);
     }
 }
--- src/main/webapp/static/js/checkout.js
+++ src/main/webapp/static/js/checkout.js
@@ -18,6 +18,26 @@ async function handleCheckoutData() {
     let expMonth = document.getElementById("expmonth").value;
     let expYear = document.getElementById("expyear").value;
     let cvv = document.getElementById("cvv").value;
+
+    //payload
+    let payload = {"userName":userName, "email":email, "address":address, "city":city, "state":state, "zipCode":zipCode,
+    "cardName":cardName, "cardNumber":cardNumber, "expMonth":expMonth, "expYear":expYear, "cvv":cvv};
+    let url = "/api/save/order";
+    const response = await apiPost(url, payload);
+}
+
+async function apiPost(url, payload) {
+
+    let response = await fetch(url, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify(payload)
+    });
+
+    return response;
 }
 
 init()
