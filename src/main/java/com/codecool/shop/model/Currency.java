78e2e6f85c52e734ed8838a2fc0d14b24564a888
SzPetra <petra.szilagyi27@gmail.com>
2022-07-27 11:53:40 +0200

    add: psql script file

--- src/main/java/com/codecool/shop/model/Currency.java
+++ src/main/java/com/codecool/shop/model/Currency.java
@@ -4,7 +4,7 @@ import java.math.BigDecimal;
 
 public enum Currency {
 
-    FORINT(new BigDecimal("0.0025")),
+    HUF(new BigDecimal("0.0025")),
     USD(new BigDecimal("0.96")),
     EUR(new BigDecimal("1"));
 
