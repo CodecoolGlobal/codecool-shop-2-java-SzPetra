bfa17fc515477d8d1c19d6c99893f4dc59249c65
cserdniel <cserdniel@hotmail.com>
2022-07-27 15:08:40 +0200

    Implement add()

--- src/main/java/com/codecool/shop/dao/jdbc/ProductCategoryDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/jdbc/ProductCategoryDaoJdbc.java
@@ -3,12 +3,36 @@ package com.codecool.shop.dao.jdbc;
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.model.ProductCategory;
 
+import javax.sql.DataSource;
+import java.sql.*;
 import java.util.List;
 
 public class ProductCategoryDaoJdbc implements ProductCategoryDao {
+
+    private DataSource dataSource;
+
+    public ProductCategoryDaoJdbc(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Override
     public void add(ProductCategory category) {
+        try(Connection connection = dataSource.getConnection()) {
+            String query = "INSERT INTO categories ( name, description, department)" +
+                    "VALUES (?, ?, ? )";
+            PreparedStatement prepStatement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            prepStatement.setString(1, category.getName());
+            prepStatement.setString(2, category.getDescription());
+            prepStatement.setString(3, category.getDepartment());
 
+            prepStatement.executeUpdate();
+
+            ResultSet rs = prepStatement.getGeneratedKeys();
+            rs.next();
+            category.setId(rs.getInt(1));
+        } catch (SQLException e) {
+            throw new RuntimeException("Could not add category to database");
+        }
     }
 
     @Override
