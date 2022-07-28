b877c92877000123ea18c2234721821501463911
SzPetra <petra.szilagyi27@gmail.com>
2022-07-28 11:39:57 +0200

    feat: stranger things themed registration page

--- src/main/java/com/codecool/shop/controller/RegistrationController.java
+++ src/main/java/com/codecool/shop/controller/RegistrationController.java
@@ -1,4 +1,29 @@
 package com.codecool.shop.controller;
 
-public class RegistrationController {
+import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.service.ProductService;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/registration"})
+public class RegistrationController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
+        WebContext context = new WebContext(request, response, request.getServletContext());
+
+        engine.process("product/registration.html", context, response.getWriter());
+    }
 }
