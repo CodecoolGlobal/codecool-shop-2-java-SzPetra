23a34ba63b5e8601a6957dac4eb0d53072971d5f
cserdniel <cserdniel@hotmail.com>
2022-07-28 16:50:25 +0200

    Change SupplierController

--- src/main/java/com/codecool/shop/controller/SupplierController.java
+++ src/main/java/com/codecool/shop/controller/SupplierController.java
@@ -1,5 +1,6 @@
 package com.codecool.shop.controller;
 
+import com.codecool.shop.config.DataSourceFactory;
 import com.codecool.shop.config.TemplateEngineUtil;
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
@@ -7,6 +8,9 @@ import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
 import com.codecool.shop.dao.implementation.SupplierDaoMem;
+import com.codecool.shop.dao.jdbc.ProductCategoryDaoJdbc;
+import com.codecool.shop.dao.jdbc.ProductDaoJdbc;
+import com.codecool.shop.dao.jdbc.SupplierDaoJdbc;
 import com.codecool.shop.service.ProductService;
 import com.codecool.shop.service.SupplierService;
 import org.thymeleaf.TemplateEngine;
@@ -17,21 +21,23 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 import java.io.IOException;
 
 @WebServlet(urlPatterns = {"/supplier/"})
 public class SupplierController extends HttpServlet {
 
+    private DataSource source = DataSourceFactory.createDataSource();
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
         int supplierId = Integer.parseInt(request.getParameter("supplier"));
 
-        ProductDao productDataStore = ProductDaoMem.getInstance();
-        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+        ProductCategoryDao productCategoryDao = new ProductCategoryDaoJdbc(source);
+        SupplierDao supplierDao = new SupplierDaoJdbc(source);
+        ProductDao productDataStore = new ProductDaoJdbc(source, productCategoryDao, supplierDao);
 
-
-        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
-        SupplierService supplierService = new SupplierService(productDataStore, supplierDataStore);
+        SupplierService supplierService = new SupplierService(productDataStore, supplierDao);
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
         WebContext context = new WebContext(request, resp, request.getServletContext());
